397eeaa13c6894e149ed38e2d83c6415390c636f
quentin-jubault <quentin.jubault2024@campus-eni.fr>
2025-07-20 16:31:27 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    direction: 'horizontal',
+    speed: 1000,
+    slidePerVuew: 1,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination-projets',
+      clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        pauseOnMouseEnter: true,
+    },
+  });
+
+  function openMenuMobile() {
+    document.querySelector('nav').classList.add('open');
+    document.querySelector('.overlay').classList.add('open');
+  }
+
+  function closeMenuMobile() {
+    document.querySelector('nav').classList.remove('open');
+    document.querySelector('.overlay').classList.remove('open');
+  }
